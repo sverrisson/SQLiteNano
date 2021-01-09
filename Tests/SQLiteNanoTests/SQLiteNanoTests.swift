8cce2c812dd68d1e3f105fa1c0cc962eef4480db
Hannes Sverrisson <hannessv@gmail.com>
2021-01-09 02:51:51 +0000

    UTF8 tests

--- Tests/SQLiteNanoTests/SQLiteNanoTests.swift
+++ Tests/SQLiteNanoTests/SQLiteNanoTests.swift
@@ -3,6 +3,33 @@ import XCTest
 
 final class SQLiteNanoTests: XCTestCase {
     
+    func testUTF() {
+        let database = SQLiteNano("TestUTF")
+        guard database.deleteAllRows() == true else {
+            fatalError("Couldn't delete all rows")
+        }
+        
+        let list = [
+        Movie(title: "3🟨🟦🟥 : Red", year: 1994),
+        Movie(title: "Boyhood👦", year: 2014),
+        Movie(title: "Citizen Kane", year: 1941),
+        Movie(title: "The 📷Godfather", year: 1972),
+        Movie(title: "Casablanca🎥", year: 1943),
+        ].shuffled()
+        guard database.storeMovies(list) >= 0 else {
+            fatalError("Could not store movies")
+        }
+        
+        let rowsCount = database.countRows()
+        XCTAssertEqual(list.count, rowsCount)
+        
+        let movie = database.findMovieFor(year: 1994)
+        print(movie)
+        let sample = list.first(where: { $0.year == 1994 })!
+        XCTAssertEqual(movie, [sample])
+        database.close()
+    }
+    
     func testExample() {
         let database = SQLiteNano("Test")
         guard database.deleteAllRows() == true else {
@@ -43,5 +70,6 @@ final class SQLiteNanoTests: XCTestCase {
 
     static var allTests = [
         ("testExample", testExample),
+        ("testUTF", testUTF),
     ]
 }
--- Tests/SQLiteNanoTests/XCTestManifests.swift
+++ Tests/SQLiteNanoTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(SQLiteNanoTests.allTests),
+        
     ]
 }
 #endif
